76e14f128d27ac2d663313cb4702d64e95323fa3
Myeongseok Baek <codetemplate@hanmail.net>
2025-01-30 20:20:00 +0900

    navigate to approved.txt done

---
+++ src/main/kotlin/pe/msbaek/approvedfilesplugin/ApprovedFileFinder.kt
@@ -0,0 +1,18 @@
+package pe.msbaek.approvedfilesplugin
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiFile
+
+class ApprovedFileFinder {
+    companion object {
+        fun findApprovedFile(method: PsiMethod): VirtualFile? {
+            val containingFile = method.containingFile.virtualFile
+            val directory = containingFile.parent
+            val className = method.containingClass?.name ?: return null
+            val methodName = method.name
+
+            val approvedFileName = "$className.$methodName.approved.txt"
+            return directory.findChild(approvedFileName)
+        }
+    }
+}
--- src/main/kotlin/pe/msbaek/approvedfilesplugin/TestMethodLineMarkerProvider.kt
+++ src/main/kotlin/pe/msbaek/approvedfilesplugin/TestMethodLineMarkerProvider.kt
@@ -1,29 +1,64 @@
 package pe.msbaek.approvedfilesplugin
+
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiManager
 
 class TestMethodLineMarkerProvider : RelatedItemLineMarkerProvider() {
     override fun collectNavigationMarkers(
         element: PsiElement,
         result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
     ) {
-        // PsiMethod인지 확인
         if (element !is PsiMethod) {
             return
         }
 
-        // @Test 어노테이션이 있는지 확인
         element.modifierList.annotations.forEach { annotation ->
             if (annotation.qualifiedName == "org.junit.jupiter.api.Test") {
-                val builder = NavigationGutterIconBuilder.create(Icons.ApprovedIcon)
-                    .setTarget(element)
-                    .setTooltipText("View approved file")
+                val approvedFile = ApprovedFileFinder.findApprovedFile(element)
+                val className = element.containingClass?.name
+                val methodName = element.name
 
-                result.add(builder.createLineMarkerInfo(element.nameIdentifier ?: element))
+                val tooltipText = if (approvedFile != null)
+                    "View approved file: ${approvedFile.name}"
+                else
+                    "Approved file not found: $className.$methodName.approved.txt"
+
+                val builder = NavigationGutterIconBuilder.create(Icons.ApprovedIcon)
+                    .setTooltipText(tooltipText)
+
+                if (approvedFile != null) {
+                    val psiFile = PsiManager.getInstance(element.project).findFile(approvedFile)
+                    if (psiFile != null) {
+                        builder.setTarget(psiFile)
+                    }
+                }
+
+                val markerInfo = builder.createLineMarkerInfo(element.nameIdentifier ?: element)
+
+                if (approvedFile == null) {
+                    showNotification(element.project, tooltipText)
+                }
+
+                result.add(markerInfo)
             }
         }
+    }
+
+    private fun showNotification(project: Project, content: String) {
+        NotificationGroupManager.getInstance()
+            .getNotificationGroup("Approved Files Plugin")
+            .createNotification(
+                "Approved File Not Found",
+                content,
+                NotificationType.WARNING
+            )
+            .notify(project)
     }
 }
