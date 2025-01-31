2530ec815719d6f6bb884f0ec5f337f80103ac38
Myeongseok Baek <codetemplate@hanmail.net>
2025-01-31 10:33:53 +0900

    add link to approvaltests in plugin.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,6 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2024.1.7")
     type.set("IC") // Target IDE Platform
